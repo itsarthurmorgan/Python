427187110e099209f9deda26dad1e4f394481ce2
itsarthurmorgan <140511073+itsarthurmorgan@users.noreply.github.com>
2023-08-17 09:38:58 +0530

    Add files via upload

--- count-char-dict.py
+++ count-char-dict.py
@@ -1,11 +1,17 @@
-str=input("Enter a String:")
-dict = {}
-for n in str:
-    keys= dict.keys()
-    print(keys)#added
-    if n in keys:
-        dict[n] = 1
-    else:
-        dict[n] = 2
-print (dict) 
-print(type(dict))#added
+def count_characters(input_string):
+    char_count = {}
+    
+    for char in input_string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    return char_count
+
+input_string = input("Enter a string: ")
+character_counts = count_characters(input_string)
+
+print("Character counts:")
+for char, count in character_counts.items():
+    print(f"'{char}': {count}")
